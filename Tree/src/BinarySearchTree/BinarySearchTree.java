8edeffb62e7cd71c78f3d2e3135c13b4792b7eca
Stefan Stoynov <sstoynov87@gmail.com>
2019-04-01 18:46:51 +0300

    commit search and delete methods

--- Tree/src/BinarySearchTree/BinarySearchTree.java
+++ Tree/src/BinarySearchTree/BinarySearchTree.java
@@ -15,7 +15,7 @@ public class BinarySearchTree<T extends Comparable<T>> {
         this.root = insert(value, null, root);
     }
 
-    private BinarySearchTreeNode<T> insert(T value, BinarySearchTreeNode<T> parent, BinarySearchTreeNode<T> node) {
+    public BinarySearchTreeNode<T> insert(T value, BinarySearchTreeNode<T> parent, BinarySearchTreeNode<T> node) {
         if (node == null) {
             node = new BinarySearchTreeNode<T>(value);
             node.setParent(parent);
@@ -28,5 +28,69 @@ public class BinarySearchTree<T extends Comparable<T>> {
             }
         }
         return node;
+    }
+
+    public BinarySearchTreeNode<T> find(T value) {
+        BinarySearchTreeNode<T> node = this.root;
+
+        while (node != null) {
+            int compareTo = value.compareTo(node.getValue());
+            if (compareTo < 0) {
+                node = node.getLeftTree();
+            } else if (compareTo > 0) {
+                node = node.getRightTree();
+            } else {
+                break;
+            }
+        }
+
+        return node;
+    }
+
+    public void remove(T value){
+        BinarySearchTreeNode<T> nodeToDelete= find(value);
+        if (nodeToDelete==null){
+            return;
+        }
+
+        remove(nodeToDelete);
+    }
+
+    public void remove(BinarySearchTreeNode<T> node){
+        if (node.getLeftTree()!=null && node.getRightTree()!=null){
+            BinarySearchTreeNode<T> replacement = node.getRightTree();
+            while (replacement.getLeftTree()!=null){
+                replacement = replacement.getLeftTree();
+            }
+            node.setValue(replacement.getValue());
+            node = replacement;
+        }
+
+        BinarySearchTreeNode<T> theChild = node.getLeftTree()!= null ? node.getLeftTree() : node.getRightTree();
+
+        if (theChild != null){
+            theChild.setParent(node.getParent());
+            if (node.getParent() == null){
+                root = theChild;
+            }else{
+                if (node.getParent().getLeftTree()==node){
+                    node.getParent().setLeftTree(theChild);
+                }else{
+                    node.getParent().setRightTree(theChild);
+                }
+            }
+
+        }else{
+            if (node.getParent() == null){
+                root = null;
+            }else{
+                if (node.getParent().getLeftTree()==null){
+                    node.getParent().setLeftTree(null);
+                }else{
+                    node.getParent().setRightTree(null);
+                }
+            }
+        }
+
     }
 }
